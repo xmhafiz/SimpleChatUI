88cfbe259c8f772c95c20f3b9fcff02cb0692dfd
Mohd Hafiz <xmhafiz@gmail.com>
2019-10-22 17:10:14 +0200

    update chat data store

--- ChatSample/Stores/ChatStore.swift
+++ ChatSample/Stores/ChatStore.swift
@@ -12,17 +12,17 @@ struct ChatStore {
     static func getAll() -> [Chat] {
         let list = [
             Chat(title: "High School Football Team, Malaysia", imageName: "1", latestMessage: "Lorem ipsum dolor sit amet", sender: "Bob"),
-            Chat(title: "Lorem Ipsum Group", imageName: "2", latestMessage: "Excepteur sint occaecat cupidatat", sender: "Fendi"),
-            Chat(title: "John Smith", imageName: "3", latestMessage: "Excepteur sint occaecat cupidatat", sender: nil),
+            Chat(title: "Lorem Ipsum Group", imageName: "2", latestMessage: "Consectetur adipiscing elit, sed do eiusmod tempor incididunt", sender: "Fendi"),
+            Chat(title: "John Smith", imageName: "3", latestMessage: "Consectetur adipiscing elit, sed do eiusmod tempor incididunt", sender: nil),
             Chat(title: "Stackoverflow", imageName: "4", latestMessage: "OK, got it 👍🏼", sender: "Mohamed Ali"),
-            Chat(title: "ABC Developers Inc", imageName: "5", latestMessage: "Excepteur sint occaecat cupidatat", sender: "You"),
-            Chat(title: "Alice", imageName: "6", latestMessage: "Consectetur adipiscing elit", sender: nil),
-            Chat(title: "Georgetown", imageName: "7", latestMessage: "Excepteur sint occaecat cupidatat", sender: "Ronaldo"),
+            Chat(title: "ABC Developers Inc", imageName: "5", latestMessage: "Consectetur adipiscing elit, sed do eiusmod tempor incididunt", sender: "You"),
+            Chat(title: "Alice", imageName: "6", latestMessage: "🎉 Consectetur adipiscing elit", sender: nil),
+            Chat(title: "Georgetown", imageName: "7", latestMessage: "Consectetur adipiscing elit, sed do eiusmod tempor incididunt", sender: "Ronaldo"),
             Chat(title: "Pak Mat Tempe", imageName: "8", latestMessage: "See ya!", sender: nil),
             Chat(title: "Hackathon Selangor", imageName: "9", latestMessage: "Thanks man!", sender: "Syafiq"),
             Chat(title: "Futsal A-Team", imageName: "10", latestMessage: "Thanks man", sender: "Falcao"),
             Chat(title: "MH Zulfadhli", imageName: "11", latestMessage: "Got new update here guys, please come here and see quickly", sender: nil),
-            Chat(title: "Ex-SK Klang", imageName: "12", latestMessage: "Excepteur sint occaecat cupidatat", sender: "Nazirah"),
+            Chat(title: "Ex-SK Klang", imageName: "12", latestMessage: "Consectetur adipiscing elit, sed do eiusmod tempor incididunt", sender: "Nazirah"),
             Chat(title: "Omak Den", imageName: "13", latestMessage: "See ya! ❤️", sender: nil),
             Chat(title: "MH Zulfadhli", imageName: "14", latestMessage: "Got new update here guys, please come here and see quickly", sender: nil)
         ]
